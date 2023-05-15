a413cf3dd1c6a92611821d1e94b2183078bbbd0e
Irina Vlad <irina.vlad99@gmail.com>
2023-05-15 15:08:21 +0300

    Login, logout si filtrare produse

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,52 @@
 import './App.css';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
-import {Route, Routes} from 'react-router-dom';
+import Login from './components/Login';
+import {Link, Route, Routes} from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const[isAuth, setIsAuth] = useState(false)
+
+  useEffect( () => {
+    const token = localStorage.getItem('token')
+    if(token){
+      setIsAuth(true);
+    } 
+
+    console.log(token) //pentru testare doar
+  }, []);
+
+  const handleLogout = () =>{
+    localStorage.removeItem('token');
+    window.location.href = '/produse';
+  }
+
   return (
-    <div>
-      <Routes>
-        <Route path="/products" element={<ProductList/>}/>
-        <Route path ="/products/:id" element={<ProductDetails/>}/>
-      </Routes>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li><Link to="/">Acasă</Link></li>
+          <li><Link to="/produse">Produse</Link></li>
+          {
+            isAuth ? (<li><button onClick={handleLogout}>Log out</button></li>)
+            :
+            (
+              <li><Link to="/login"><button>Log in</button></Link></li>
+            )
+          }
+          <li><Link to="/cos">Coșul tău</Link></li>
+        </ul>
+      </nav>
+      <div>
+        <Routes>
+          <Route path="/produse" element={<ProductList/>}/>
+          <Route path ="/produse/:id" element={<ProductDetails/>}/>
+          <Route path="/login" element={<Login/>}></Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -6,27 +6,46 @@ import API from '../services/API';
 
 function ProductList() {
     const [products, setProducts] = useState([])
+    const [categories, setCategories] = useState([])
+
+    const[selectedCategory, setSelectedCategory] = useState('')
+    const[filteredProducts, setFilteredProducts] = useState(products)
 
     useEffect( () => {
         API.getAllProducts().then(data => {
             console.log(data.produse)
             setProducts(data.produse)
+
+            const uniqueCategories = [...new Set(data.produse.map(product => product.categorie))];
+            setCategories(uniqueCategories)
         })
     }, []);
+
+    const handleCategoryChange = e => setSelectedCategory(e.target.value);
+
+    useEffect( () => {
+      const updatedProducts = selectedCategory
+            ? products.filter(product => product.categorie === selectedCategory)
+            : products;
+
+            setFilteredProducts(updatedProducts);
+    }, [selectedCategory, products]);
 
   return (
     <div> 
       <h1>Lista produse</h1>
       {/* <button><Link to={`/products/create`}>Add product</Link></button> */}
-      <ul>
-        {products.map(product => {
-          return(
-            <li key = {product.id}>
-              <h3><Link to={`/products/${product.id}`}>{product.nume}</Link></h3>
-            </li>
-          )
-        })}
-      </ul>
+      <select value={selectedCategory} onChange={handleCategoryChange} className="form-control w-25">
+      <option value="">Toate categoriile</option>
+      {categories.map((category, index) => (
+        <option key={index} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+      {filteredProducts.map(product => (
+        <div key={product.id}><Link to={`/produse/${product.id}`}>{product.nume}</Link></div>
+      ))}
       
     </div>
   )
