dc1a9f3edd815ca720f38057c903ed8eefc69fbd
Irina Vlad <irina.vlad99@gmail.com>
2023-06-02 18:14:09 +0300

    Modificare logica plasare comanda

--- frontend/src/components/OrderDetails.js
+++ frontend/src/components/OrderDetails.js
@@ -1,10 +1,43 @@
 import React from 'react'
 import { useState } from 'react'
 
+
 function OrderDetails() {
+  const [userAddressCheck, setUserAddressCheck] = useState(false);
+  const [newAddressCheck, setNewAddressCheck] = useState(false);
+
+  const handleUserAddress = () => {
+    if (!userAddressCheck){
+      setUserAddressCheck(true);
+      //interogheaza pentru datele utilizatorului autentificat
+    }else{
+      setUserAddressCheck(false);
+    }
+  }
+
+  const handleNewAddress = () => {
+    if(!newAddressCheck){
+      setNewAddressCheck(true);
+      //formular
+    }else{
+      setNewAddressCheck(false);
+    }
+  }
+  
+
   return (
     <div>
-      
+      <h2>Adresa de livrare</h2>
+      <form>
+        <label>
+          <input type='checkbox' checked={userAddressCheck} onChange={handleUserAddress}/>
+          Adresa de domiciliu
+        </label>
+        <label>
+          <input type='checkbox' checked={newAddressCheck} onChange={handleNewAddress}/>
+          Altă adresa de livrare
+        </label>
+      </form>
     </div>
   )
 }
